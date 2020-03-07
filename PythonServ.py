11012411aac3ce0a145ca07f02dc76bd786e3807
Less-Than-Perfect <Ozzymeadows@tutanota.com>
2020-03-06 20:07:46 -0600

    I finally setup my workspace, so this commit contains alot of changes (as I havn't commited frequently enough)
    I primarily added a system to check the length of PKCS#7 padding (which hopefully is now fully functional). I plan on finishing the rest of the primary function soon (aka, the actual enumeration).

--- PythonServ.py
+++ PythonServ.py
@@ -1,4 +1,4 @@
-#!/bin/python3
+#!/usr/bin/python3
 # Sources: https://palletsprojects.com/p/flask/, https://gist.github.com/forkd/168c9d74b988391e702aac5f4aa69e41, https://flask.palletsprojects.com/en/1.1.x/quickstart/#a-minimal-application
 
 from hashlib import md5
@@ -9,29 +9,37 @@ from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad, unpad
 import traceback
 from flask import Flask, escape, request
+import urllib.parse
+from sys import stderr
+import logging
 
 app = Flask(__name__) # Figure out why I need this
 
 @app.route("/")
 def hello_world():
-    msg = "happyerbdayhenryfordhuhlolhappybffforeverandeverman"
-    pwd = "personhadasandwitchforbreakfastontheinternettoday202"
+    msg = "ilisgingspariesuranizestanedgelishearionzarriceaa5"
+    pwd = "irdeitusablidessesssewallutordandixialstrildippor3"
     key = md5(pwd.encode('utf8')).digest()
 
     cte = request.args.get("cte" )
+    if (cte):
+        cte = urllib.parse.unquote(cte)
     #pwds = request.args.get("pwd" )
-    try:
-        if (cte): # decyrpt
-            key = md5(pwd.encode('utf8')).digest()
-            raw = b64decode(cte)
-            cipher = AES.new(key, AES.MODE_CBC, raw[:AES.block_size])
-            return unpad(cipher.decrypt(raw[AES.block_size:]), AES.block_size).decode('utf-8')
+
+    if (cte): # decyrpt
+        key = md5(pwd.encode('utf8')).digest()
+        raw = b64decode(cte)
+        cipher = AES.new(key, AES.MODE_CBC, raw[:AES.block_size])
+        print (cipher.decrypt(raw[AES.block_size:]))
+        rValue = unpad(cipher.decrypt(raw[AES.block_size:]), AES.block_size).decode('utf-8')
+        return rValue
         
-        else: # encrypt
-            iv = "1234567890123456"
-            cipher = AES.new(key, AES.MODE_CBC, iv)
-            return b64encode(iv + cipher.encrypt(pad(msg.encode('utf-8'), 
-                AES.block_size))).decode('utf-8')
-    except Exception:
-        #return traceback.print_exc() 
-        return "bad"
+    else: # encrypt
+        iv = get_random_bytes(AES.block_size)
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        lolValue = b64encode(iv + cipher.encrypt(pad(msg.encode('utf-8'),  AES.block_size))).decode('utf-8')
+        return urllib.parse.quote_plus(lolValue)
+        
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- main.pl
+++ main.pl
@@ -1,62 +1,133 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-use HTTP::Tiny; # https://perldoc.perl.org/HTTP/Tiny.html
-use MIME::Base64; # remove this from the namespace
-use URI::Escape; #  http://search.cpan.org/perldoc/URI::Escape
+use HTTP::Tiny;# https://perldoc.perl.org/HTTP/Tiny.html
+use MIME::Base64; 
+use URI::Escape;#  http://search.cpan.org/perldoc/URI::Escape
 use Encode qw(decode encode);
+use List::MoreUtils qw(any);
 
 print "Ich beginne\n";
 
 my $bSize = 16;
 
-my $wante = 'PKCS#7 padding';
+my $AIM = 0;
 
-my $parm = 'G%2BglEae6W%2F1XjA7vRm21nNyEco%2Fc%2BJ2TdR0Qp8dcjPJ3JtkSaJRrJwlP%2BDsbHXlYKSh%2FPMVHnhLmbzHIY7GAR1bVcy3Ix3D2Q5cVi8F6bmY%3D';
+my @wante = ('ValueError: Padding is incorrect.', 'ValueError: PKCS#7 padding is incorrect.');
+
+my $parm = 'sn7DK7gTuLUp2NRBzN8PRejnx1HscfJWvc2U9UaspIdsJX0OAizXs%2BlHR%2BnX%2BxxpohDZC%2BRkDOiLTzeWlWGzEgFaccOxG%2F2N5f6w5kbGjvo%3D';
+#my $parm = 'G%2BglEae6W%2F1XjA7vRm21nNyEco%2Fc%2BJ2TdR0Qp8dcjPJ3JtkSaJRrJwlP%2BDsbHXlYKSh%2FPMVHnhLmbzHIY7GAR1bVcy3Ix3D2Q5cVi8F6bmY%3D';
 #my $parm = 'ULp-Dd93!!pZmzC6q7CnFh9AOMB6NdDJpT5FtFwdPAU4ctEYKuzA14gvyHodr60GOgXDSDIm2PFrp5jLvlQS5ofQGpTrPoQDGq4yl25-QEgSOsYTzV9XidDTHtXncxYmz25bKUDqCz3KZSI0xdbMcyRI7lxDWwtR5MLu!RHzANbyyZwnY037m66XCA-XjMzJbheQcjc5O5yLfLW9iwFDNA~~';
 
 #$parm =~ s/-/+/ig;
 #$parm =~ s/!/\//ig;
 #$parm =~ s/~/=/ig;
 
-# Find out if I can redact something automatically with git
-my $url = 'http://natas28:REDACTED@natas28.natas.labs.overthewire.org/search.php/?query='; # Maybe I'm just being silly and natas28 doesn't even use cbc...
-#my $url = 'REDACTED';
-#
+my $url = 'http://127.0.0.1:5000/?cte=';
+
+#my $url = 'http://natas28:JWwR438wkgTsNKBbcJoowyysdM82YjeF@natas28.natas.labs.overthewire.org/search.php/?query=';
+
+#my $url = 'http://35.227.24.107/e4fe34027d/?post=';
 
 $parm = uri_unescape($parm);
 
 $parm = decode_base64($parm);
 
-my $qLen = length($parm);
+my $qLen = length($parm)-1;
 
-# A byte check (still figuring out how the decode/encode module works)
-my $octets = decode("UTF-8", $parm);
-print $octets."\n\n";
+print "String Length: ".$qLen."\n";
+
+#my $octets = decode("UTF-8", $parm);
+#print $octets."\n";
+
+# Padding length
+my $tempPARM = $parm;
+my $it = 6;
+my $currentB = $qLen-($bSize+$it);
+
+substr($tempPARM, $currentB, 1) = chr(ord(substr($tempPARM, $currentB, 1))^255);
+my $return = getRequest($tempPARM);
+my $limit = 0;
+if ($return == 1){$it = 0; $limit = 5;}else{$it = $it + 2; $limit = 17;} # edit this to reflect changes of the #bSize variable
+
+while ($it < $limit){
+    $tempPARM = $parm;
+    $currentB = $qLen-($bSize+$it);
+
+    substr($tempPARM, $currentB, 1) = chr(ord(substr($tempPARM, $currentB, 1))^255);
+    $return = getRequest($tempPARM);
+    if ($return == 1){ # Test  if using a ($return) statment works
+        if ($it == 0){
+            $AIM = $it;
+        }else{
+            $tempPARM = $parm;
+            $it--;
+            $currentB = $qLen-($bSize+$it);
+
+            substr($tempPARM, $currentB, 1) = chr(ord(substr($tempPARM, $currentB, 1))^255);
+            $return = getRequest($tempPARM);
+            if ($return == 1){
+                $AIM = $it; #Maybe do some inital arithmetic here
+            }else{
+                $AIM = $it+1; #Maybe do some inital arithmetic here
+            }
+        }
+        last;
+    }else{
+        $it = $it + 2;
+    }
+}
+if ($it == 18){
+    $it = $bSize; #Maybe do some inital arithmetic here
+}
 #
 
-my $it = 0;
-while($it < 16){
-    my $currentB = $qLen-($bSize+$it);
+print "\n$AIM\n\n";
+
+sub getRequest {
+    my $heel = uri_escape(encode_base64($_[0], ''));
+    print "$heel\n";
+    my $response = HTTP::Tiny->new->get($url.$heel);
+    if (not any { $response->{content} =~ $_ } @wante){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+
+exit();
+
+
+$it = 0;
+while($it < $bSize){
+    $currentB = $qLen-($bSize+$it);
     my $byte = ord(substr($parm, $currentB, 1)) ;
     my $whBYTE = $byte;
     $byte++;
-    print $it."\n";
+    print "it = ".$it."\n";
     while($byte != $whBYTE){
+        my $cAIM = substr($parm, $currentB, 1);
         substr($parm, $currentB, 1) = chr($byte);
         my $tempQ =  uri_escape(encode_base64($parm, ''));
         #my $tempQ = encode_base64($parm, '');
         #$tempQ =~ s/\+/-/ig;
         #$tempQ =~ s/\//!/ig;
         #$tempQ =~ s/=/~/ig;
-        my $response = HTTP::Tiny->new->get($url.$tempQ);
-       if (not grep(/$wante/, $response->{content})){
-            print $byte."\n".$response->{content}."\n";
-            last
+        $response = HTTP::Tiny->new->get($url.$tempQ);
+        if (not any { $response->{content} =~ $_ } @wante){
+            my $AIM = 1;
+            print $byte."\n".$url.$tempQ."\n";
+            print "$byte, ".ord($cAIM)." $AIM\n";
+            my $xD = $byte^ord($cAIM)^$AIM;
+            print "Plain Text: ".$xD."\n";
+            exit();
+            last;
         }else{
-            print "Nope: $byte\n\n";
+            print $byte." ";
         }
         $byte = ($byte + 1)%256;
     }
     $it++;
+}
+print "\n";
 }
